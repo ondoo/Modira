9b0a1da0c93f0d7dde7097a60e013c6819419445
whtnqls124578 <whtnqls124578@naver.com>
2022-08-09 18:06:42 +0900

    카카오로그인까지

---
+++ src/components/Oauth.js
@@ -0,0 +1,11 @@
+// OAuth.js 라는 컴포넌트를 따로 생성하여 관리하였음
+// Kakao 로그인 rest api / REDIRECT_URI 모은 인가코드
+
+// kakao 인가코드
+// 틀 : "https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code"
+// 다 넣은거 : "https://kauth.kakao.com/oauth/authorize?client_id={ddb938f8fed6079e90564fca875e2903}&redirect_uri={localhost:3000/oauth/callback/kakao}&response_type=code"
+
+const CLIENT_ID = "ddb938f8fed6079e90564fca875e2903";
+const REDIRECT_URI =  "http://localhost:3000/oauth/callback/kakao";
+
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { KAKAO_AUTH_URL } from "../components/Oauth";
+
+// kakao 로그인 참고 : https://data-jj.tistory.com/53
 
 const Login = () => {
+
   return (
-    <div>Login</div>
+    <div className="login">
+      <a className="kakao" href={KAKAO_AUTH_URL}>
+        네이버
+      </a>
+      <button>페이스북</button>
+      <button>카카오톡</button>
+      <button>구글</button>
+    </div>
   )
 }
 export default Login
