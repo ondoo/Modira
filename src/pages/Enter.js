e88efc78af07d95e48eb15d4cf219444683b272b
ondoo <leh237@gmail.com>
2022-08-09 15:49:03 +0900

    firstlogin,enter페이지 추가

---
+++ src/pages/Enter.js
@@ -0,0 +1,6 @@
+const Enter = () => {
+  return (
+    <div>Enter</div>
+  )
+}
+export default Enter
--- src/pages/FirstLogin.js
+++ src/pages/FirstLogin.js
@@ -1,6 +1,25 @@
+import styled from "styled-components";
+
+
 const FirstLogin = () => {
   return (
-    <div>FirstLogin</div>
-  )
-}
-export default FirstLogin
+    <div>
+      <Box>
+        {" "}
+        <button>뒤로가기</button>
+        <button>프로필 사진 추가</button>
+      </Box>
+
+      <p>이름 </p>
+    </div>
+  );
+};
+export default FirstLogin;
+
+const Box = styled.div`
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
