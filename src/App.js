9f697a37f2ffa1b8247566cf4d8b318be2b9193a
youdaehoon <107843412+youdaehoon@users.noreply.github.com>
2022-08-09 15:42:13 +0900

    abbweb css first setting

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ import Post from "./pages/Post";
 import Write from "./pages/Write";
 import FirstLogin from "./pages/FirstLogin";
 import MyInfo from "./pages/MyInfo";
-
+import styled from"styled-components"
+import testimage from"../src/image/11.jpg"
 
 
 function App() {
   return (
     <>
-    <Navbar/>
-      <Routes>
+     <Navbar/>
+    <Background>
+  <Test2>나는 없어질 애</Test2>
+    <Test1> <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
@@ -26,8 +29,52 @@ function App() {
         <Route path="/myinfo" element={<MyInfo />} />
         <Route path="/firstlogin" element={<FirstLogin />} />
       </Routes>
+      </Test1>
+  </Background>
+   
+     
     </>
   );
 }
 
 export default App;
+
+const Background=styled.div`
+width: 100%;
+height: 500px;
+background-image: url(${testimage});
+background-repeat: no-repeat;
+background-size:  100%;
+
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+@media screen and (max-width: 900px){
+ background-image: none;
+ justify-content:center;
+}
+
+`
+const Test2=styled.div`
+@media screen and (max-width: 900px){
+  display: none;
+  
+}
+  width: 40%;
+  height: 100%;
+  background-color: blue;
+
+`
+const Test1=styled.div`
+@media screen and (max-width: 900px){
+  width: 400px;
+  margin-right:0px;
+
+
+}
+  width: 400px;
+  height: 100%;
+  background: green;
+  margin-right:50px;
+
+`
