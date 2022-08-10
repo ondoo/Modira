2eeffbea2cfa48432a487e014d36883e8c4e1ec2
whtnqls124578 <whtnqls124578@naver.com>
2022-08-10 22:13:56 +0900

    소셜로그인 인가코드 콘솔에 저장

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 
 const Main = () => {
 
-  let [kakaoLogIn,setKakaoLogIn] = useState('');
-
     // kakao/naver 인가 뽑아오기 (백엔드에 보낼 인가코드) 
     let code = new URL(window.location.href).searchParams.get("code");
     console.log(code)
